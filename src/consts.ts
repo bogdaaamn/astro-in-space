0cba1c00b91167abfe0e392683c5877cd6388ab1
Bogdan Covrig <bogcov@gmail.com>
2023-03-09 19:59:39 +0100

    chore: Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "This is an Astro blog on Space";
+export const SITE_DESCRIPTION = "Welcome in Space, Astro!";
